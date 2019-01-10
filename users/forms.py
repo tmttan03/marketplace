65574de23fca552eb5c4e74043d63dc90a630d6d
tmttan03 <tmttan03@gmail.com>
2019-01-10 15:27:51 +0800

    Email Validation

--- users/forms.py
+++ users/forms.py
@@ -2,12 +2,23 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Profile
+from django.core.validators import validate_email
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     first_name = forms.TextInput()
     last_name = forms.TextInput()
+
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        try:
+            match = User.objects.get(email=email)
+            raise forms.ValidationError('This email address is already in use.')
+        except User.DoesNotExist:
+            return email
+        
 
     class Meta:
         model = User
