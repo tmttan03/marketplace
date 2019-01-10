18e64c80ca8863c6886b77003c5190f61c009197
tmttan03 <tmttan03@gmail.com>
2019-01-10 15:01:27 +0800

    User Restriction

--- users/views.py
+++ users/views.py
@@ -5,16 +5,19 @@ from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
-	if request.method == 'POST':
-		form = UserRegisterForm(request.POST)
-		if form.is_valid():
-			form.save()
-			username = form.cleaned_data.get('username')
-			messages.success(request, f'Your account has been created! You are now able to login.')
-			return redirect('login')
+	if not request.user.is_authenticated:
+		if request.method == 'POST':
+			form = UserRegisterForm(request.POST)
+			if form.is_valid():
+				form.save()
+				username = form.cleaned_data.get('username')
+				messages.success(request, f'Your account has been created! You are now able to login.')
+				return redirect('login')
+		else:
+			form = UserRegisterForm()
+		return render(request, 'users/register.html',{'form': form})
 	else:
-		form = UserRegisterForm()
-	return render(request, 'users/register.html',{'form': form})
+		return redirect('post-home')
 
 
 @login_required
