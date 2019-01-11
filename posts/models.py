f687834b03b4522f768771afca7d007837ed0576
tmttan03 <tmttan03@gmail.com>
2019-01-11 09:51:31 +0800

    Added Status Field in Products

--- posts/models.py
+++ posts/models.py
@@ -10,12 +10,24 @@ class Category(models.Model):
 		return self.title
 
 class Product(models.Model):
+	INACTIVE = '0'
+	ACTIVE = '1'
+
+	STATUS_CHOICES = (
+		(INACTIVE, "Inactive"),
+		(ACTIVE, "Active"),
+	)
+
 	name = models.CharField(max_length=100)
 	description = models.TextField()
 	price = models.DecimalField(max_digits=10, decimal_places=2)
 	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
-	
+	status = models.CharField(
+        max_length=2,
+        choices=STATUS_CHOICES,
+        default=ACTIVE,
+    )
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
