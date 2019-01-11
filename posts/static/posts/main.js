de74b118d0c41a8b3f86e1d5e63154566cb867aa
tmttan03 <tmttan03@gmail.com>
2019-01-11 10:27:52 +0800

    Altered Static files for Update Post Form

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -25,6 +25,44 @@ $(document).on('submit', '#post-create-form', function (e){
 });
 
 
+/* Update Post */
+$("a[id='edit-btn']").click(function() {
+  var link = $(this).attr('href');
+/*Detail Modal*/
+  $('#update_post_modal').on('show.bs.modal', function (e) {
+    //var link = $("#detail-card").attr('href');
+    console.log(link);
+    $.ajax({
+          url: link,
+          method: "GET",
+          success: function(response){
+            //console.log(response);
+            $('#update_post_modal').attr('data-url', link);
+            $('#update-post').html(response);
+          }
+     })
+  });
+ });
+
+$("a[id='edit-btn']").click(function() {
+    var url = $(this).attr('href');
+$(document).on('submit', '#UpdateForm', function (e){
+    //var url = $(this).attr("action");
+    e.preventDefault();
+     $.ajax({
+        url: url,
+        method: "POST",
+        data:  $(this).serialize(),
+        success: function(response){
+            $('#update-post').html(response);
+        }
+   })
+});
+});
+
+
+
+
 $("a[id='detail-card']").click(function() {
   var link = $(this).attr('href');
 /*Detail Modal*/
