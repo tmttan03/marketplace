af1d5f43ad7e382c8d60fdfdd53aab683a162e13
tmttan03 <tmttan03@gmail.com>
2019-01-11 09:52:37 +0800

    Added Additional Fields for Form Model

--- posts/forms.py
+++ posts/forms.py
@@ -3,13 +3,13 @@ from .models import Product, Category
 from django.contrib.auth.models import User
 
 class PostForm(forms.ModelForm):
-	name = forms.CharField(max_length=100)
-	description = forms.CharField(widget=forms.Textarea)
-	price = forms.DecimalField(max_digits=10, decimal_places=2)
-	category = forms.ModelChoiceField(Category.objects.all())
+	name = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={'placeholder':'What are you selling?'}))
+	description = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Describe your item', 'rows':'3'}), label='')
+	price = forms.DecimalField(max_digits=10, decimal_places=2, label='', widget=forms.TextInput(attrs={'placeholder':'Price'}))
+	category = forms.ModelChoiceField(Category.objects.all(), label='')
 
 	class Meta:
 		model = Product
 		fields = ['name','price','category','description']
 
-
+ 
