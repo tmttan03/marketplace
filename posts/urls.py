c16a6e1ef8701f502778e5b5a6817a610429e244
tmttan03 <tmttan03@gmail.com>
2019-01-11 09:55:42 +0800

    Create Post Form

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import PostListView, UserProductsListView
+from .views import UserProductsListView , PostListView, PostView, DetailView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-home'),
+    path('create/', PostView.as_view(), name='post-create'),
+    path('item/<int:pk>/', DetailView.as_view(), name='view-post'),
+    #path('update/<int:pk>/', UpdateView.as_view(), name='post-update'),
     path('selling/<str:username>', UserProductsListView.as_view(), name='user-products'),
-    path('new/', views.create_post, name="create_post"),
+
 ]
