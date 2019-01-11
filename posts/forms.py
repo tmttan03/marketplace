d1d2128a5d45858051b7cef371feab470f78d1dc
tmttan03 <tmttan03@gmail.com>
2019-01-11 10:12:49 +0800

    Added Update Form

--- posts/forms.py
+++ posts/forms.py
@@ -12,4 +12,13 @@ class PostForm(forms.ModelForm):
 		model = Product
 		fields = ['name','price','category','description']
 
- 
+class UpdatePostForm(forms.ModelForm):
+	name = forms.CharField(max_length=100)
+	description = forms.CharField(widget=forms.Textarea)
+	price = forms.DecimalField(max_digits=10, decimal_places=2)
+	category = forms.ModelChoiceField(Category.objects.all())
+
+
+	class Meta:
+		model = Product
+		fields = ['name','price','category','description'] 
