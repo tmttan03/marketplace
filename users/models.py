d5fe2bd5174269f575abe8735df49d879bb5e0e1
tmttan03 <tmttan03@gmail.com>
2019-01-30 13:42:51 +0800

    Users connect to stripe

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -16,3 +18,19 @@ class Profile(models.Model):
     		output_size = (300,300)
     		img.thumbnail(output_size)
     		img.save(self.image.path)
+
+
+class userStripe(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    stripe_id = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Stripe Account'
+
+    def __unicode__(self):
+        if self.stripe_id:
+            return str(self.stripe_id)
+        else:
+            return self.user.username
+
+
--- users/signals.py
+++ users/signals.py
@@ -1,8 +1,11 @@
+import stripe
+
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Profile
+from .models import Profile, userStripe
 
+stripe.api_key = "sk_test_LWtBD1TOvlfIMzdIawpJvHzj"
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
@@ -10,6 +13,26 @@ def create_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
 
 
+
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
 	instance.profile.save()
+
+
+@receiver(post_save, sender=User)
+def stripeCallback(sender, instance, **kwargs):
+    user_stripe_account, created = userStripe.objects.get_or_create(user=instance)
+    if created:
+        print(f'{instance.username} Stripe')
+    if user_stripe_account.stripe_id is None or user_stripe_account.stripe_id == '':
+        new_stripe_id = stripe.Customer.create(email=instance.email)
+        user_stripe_account.stripe_id = new_stripe_id['id']
+        user_stripe_account.save()
+
+
+@receiver(post_save, sender=User)
+def profileCallback(sender, instance, **kwargs):
+    userProfile, is_created = Profile.objects.get_or_create(user=instance)
+    if is_created:
+        userProfile.name = user.username
+        userProfile.save()
